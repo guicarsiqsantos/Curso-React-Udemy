39ea481ce7c25783f2e949d9611fcf8a157261df
guicarsiqsantos <guicarsiqsantos@gmail.com>
2023-07-04 11:32:22 -0300

    Consumindo states do jogo

--- 6_SECRET_WORD-PROJETO/src/components/Game.js
+++ 6_SECRET_WORD-PROJETO/src/components/Game.js
@@ -1,18 +1,35 @@
 import "./Game.css";
 
-const Game = ({ verifyLetter }) => {
+const Game = ({
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
+}) => {
   return (
     <div className="game">
       <p className="points">
-        <span>Pontuação: 000</span>
+        <span>Pontuação: {score}</span>
       </p>
       <h1>Advinhe a palavra:</h1>
       <h3 className="tip">
-        Dica sobre a palavra: <span>Dica...</span>
+        Dica sobre a palavra: <span>{pickedCategory}</span>
       </h3>
+      <p>Você ainda tem {guesses} tentativas(S).</p>
       <div className="wordContainer">
-        <span className="letter">A</span>
-        <span className="blankSquare"></span>
+        {letters.map((letter, i) =>
+          guessedLetters.inCluedes(letter) ? (
+            <span key={i} className="letter">
+              {letter}
+            </span>
+          ) : (
+            <span key={i} className="blankSaquare"></span>
+          )
+        )}
       </div>
       <div className="letterContainer">
         <p>Tente advinhar uma letra da palavra:</p>
