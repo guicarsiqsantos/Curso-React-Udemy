28271e9fa3a0e196c32a3256d4898eca1caa70dc
guicarsiqsantos <guicarsiqsantos@gmail.com>
2023-07-11 08:12:36 -0300

    Enviando dados para formulado

--- 7_REQ_HTTP/src/App.js
+++ 7_REQ_HTTP/src/App.js
@@ -2,25 +2,92 @@ import "./App.css";
 
 import { useState, useEffect } from "react";
 
+// 4 - Custom hooks
+import { useFetch } from "./hooks/useFetch";
+
 const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
 
+  // 4 - Custom hook
+  const { data: items, httpConfig } = useFetch(url);
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
   //1 - resgatando dados
-  useEffect(async () => {
-    const res = await fetch(url);
+  // useEffect(() => {
+  //   async function FetchData() {
+  //     const res = await fetch(url);
 
-    const data = await res.json();
+  //     const data = await res.json();
 
-    setProducts(data);
-  }, []);
+  //     setProducts(data);
+  //   }
+  //   FetchData();
+  // }, []);
 
-  console.log(products);
+  // 2 - add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+
+    // 3 - carrega dinamico
+    const addedProduct = await res.json();
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    setName("");
+    setPrice("");
+  };
 
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+      <ul>
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+            </li>
+          ))}
+      </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </div>
   );
 }
---
+++ 7_REQ_HTTP/src/hooks/useFetch.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+// 4 - custom hook
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  // 5 - refatorando post
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
+
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method,
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(url);
+
+      const json = await res.json();
+
+      setData(json);
+    };
+
+    fetchData();
+  }, [url, callFetch]);
+
+  // 5 - refatorando post
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === "POST") {
+        let fetchOptions = [url, config];
+
+        const res = await fetch(...fetchOptions);
+
+        const json = await res.json();
+
+        setCallFetch(json);
+      }
+    };
+    httpRequest();
+  }, [config]);
+
+  return { data, httpConfig };
+};
