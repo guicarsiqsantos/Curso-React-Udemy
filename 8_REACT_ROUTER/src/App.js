1edb9779bf93c00117fa31e5ca7720f2a8948b43
guicarsiqsantos <guicarsiqsantos@gmail.com>
2023-07-17 08:31:13 -0300

    Finalizção do react router

--- 8_REACT_ROUTER/src/App.js
+++ 8_REACT_ROUTER/src/App.js
@@ -1,22 +1,47 @@
 import "./App.css";
 
-// 1 - config react router
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// 1 - config react router, sem links
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 
-export default function App() {
+// 2 - adicionando links
+// components
+import Navbar from "./components/Navbar";
+import Product from "./pages/Product";
+import Info from "./pages/Info";
+import NotFound from "./pages/NotFound";
+import Search from "./pages/Search";
+import { SearchForm } from "./components/SearchForm";
+
+function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar />
+        {/* 9 - search */}
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="about" element={<About />} />
+          {/* 6 - nested route */}
+          <Route path="/products/:id/info" element={<Info />} />
+          {/* 4 - rota dinamica */}
+          <Route path="/products/:id" element={<Product />} />
+
+          {/* 9 search params */}
+          <Route path="/search" element={<Search />} />
+          {/* 10 - redirect */}
+          <Route path="/company" element={<Navigate to="/about" />} />
+          {/* 7  - no match route */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
+
+export default App;
