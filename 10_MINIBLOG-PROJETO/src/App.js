d49a576f858d8f4da3fd452fd5cdfbbec5e7753a
guicarsiqsantos <guicarsiqsantos@gmail.com>
2023-09-22 09:14:32 -0300

    Adicionando a biblioteca react-router-dom

--- 10_MINIBLOG-PROJETO/src/App.js
+++ 10_MINIBLOG-PROJETO/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "./pages/Home/home";
+import About from "./pages/About/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ 10_MINIBLOG-PROJETO/src/pages/About/About.js
@@ -0,0 +1,12 @@
+import styles from "./About.module.css";
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
