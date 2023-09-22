a4c7d1ceed60cfb9e0869c9a6c035aafb9722649
guicarsiqsantos <guicarsiqsantos@gmail.com>
2023-09-22 08:18:33 -0300

    Adicionando banco de dados firebase no projeto

---
+++ 10_MINIBLOG-PROJETO/Data/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJH3lPziB8sdN-GrYPz2SjWXmsON1UnNY",
+  authDomain: "miniblog-59225.firebaseapp.com",
+  projectId: "miniblog-59225",
+  storageBucket: "miniblog-59225.appspot.com",
+  messagingSenderId: "1091662051651",
+  appId: "1:1091662051651:web:e866fbf6fc382209cc57f6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
