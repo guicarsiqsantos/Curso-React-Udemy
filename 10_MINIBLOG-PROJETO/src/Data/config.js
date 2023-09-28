9303851b50fb8a96ad10281019426853528c7a26
guicarsiqsantos <guicarsiqsantos@gmail.com>
2023-09-28 09:28:43 -0300

    criando os metados para registrar usuário no sistema

--- 10_MINIBLOG-PROJETO/src/Data/config.js
+++ 10_MINIBLOG-PROJETO/src/Data/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCJH3lPziB8sdN-GrYPz2SjWXmsON1UnNY",
--- 10_MINIBLOG-PROJETO/src/hooks/useAuthentication.js
+++ 10_MINIBLOG-PROJETO/src/hooks/useAuthentication.js
@@ -1,3 +1,5 @@
+import { db } from "../Data/config";
+
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -5,3 +7,70 @@ import {
   updateProfile,
   signOut,
 } from "firebase/auth";
+
+import { useState, useEffect } from "react";
+
+export const useAuthentication = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const [cancelled, setCancelled] = useState(false);
+
+  const auth = getAuth();
+
+  function checkIfIsCancelled() {
+    if (cancelled) {
+      return;
+    }
+  }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(null);
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+
+      setLoading(false);
+
+      return user;
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+
+      let systemErrorMessage;
+
+      if (error.message.includes("Password")) {
+        systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres.";
+      } else if (error.message.includes("email-already")) {
+        systemErrorMessage = "Email já cadastrado.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+      }
+
+      setLoading(false);
+      setError(systemErrorMessage);
+    }
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    loading,
+  };
+};
