01d00149722224e51ea2a5cb8043f51a4b59191b
guicarsiqsantos <guicarsiqsantos@gmail.com>
2023-10-19 11:17:12 -0300

    Bloqueando páginas que precisam de autenticação

--- 10_MINIBLOG-PROJETO/src/pages/About/About.js
+++ 10_MINIBLOG-PROJETO/src/pages/About/About.js
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
 import styles from "./About.module.css";
 import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Blog</span>
+      </h2>
+      <p>
+        Este projeto consiste em um blog feito com React no Front-End e Fire
+        Base no Back-End.
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar Post
+      </Link>
     </div>
   );
 };
