84f556770dbb3a286aa8ba21cbd48e8f62e6b4f7
guicarsiqsantos <guicarsiqsantos@gmail.com>
2023-10-20 08:46:21 -0300

    Login de usuário

--- 10_MINIBLOG-PROJETO/src/pages/Login/Login.js
+++ 10_MINIBLOG-PROJETO/src/pages/Login/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const { createUser, error: authError, loading } = useAuthentication();
+  const { login, error: authError, loading } = useAuthentication();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,7 +22,7 @@ const Login = () => {
       password,
     };
 
-    const res = await createUser(user);
+    const res = await login(user);
 
     console.log(res);
   };
